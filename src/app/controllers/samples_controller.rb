860544ad256263e3f04b956f4e2f31de24fa57ed
seiichi0404 <yamayamaseisei1834@gmail.com>
2024-04-23 20:42:55 +0900

    first commit

---
+++ src/app/controllers/samples_controller.rb
@@ -0,0 +1,5 @@
+class SamplesController < ApplicationController
+    def index
+        
+    end
+end
---
+++ src/test/models/article_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+
+class ArticleTest < ActiveSupport::TestCase
+  def test_the_truth
+    assert true
+  end
+
+  # test "should not save article without title" do
+  #   article = Article.new
+  #   assert_not article.save
+  # end
+end
