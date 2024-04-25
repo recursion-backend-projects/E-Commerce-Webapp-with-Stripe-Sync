4d99625da4924a813cf305c021b70fd451fc9688
AkinoJoey <gakiya213@gmail.com>
2024-04-25 17:05:33 +0900

    flowbiteを追加

--- src/config/tailwind.config.js
+++ src/config/tailwind.config.js
@@ -1,23 +1,25 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: [
-    "./public/*.html",
-    "./app/helpers/**/*.rb",
-    "./app/javascript/**/*.js",
-    "./app/views/**/*.{html.erb,haml,html,slim}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  plugins: [
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/container-queries"),
-  ],
+	content: [
+		"./public/*.html",
+		"./app/helpers/**/*.rb",
+		"./app/javascript/**/*.js",
+		"./app/views/**/*.{html.erb,haml,html,slim}",
+		"./node_modules/flowbite/**/*.js",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	plugins: [
+		require("@tailwindcss/forms"),
+		require("@tailwindcss/aspect-ratio"),
+		require("@tailwindcss/typography"),
+		require("@tailwindcss/container-queries"),
+		require("flowbite/plugin"),
+	],
 };
