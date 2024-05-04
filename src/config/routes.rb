ade943fc3cadf7894ca30cc4ea057bd852bd94a5
seiichi0404 <yamayamaseisei1834@gmail.com>
2024-05-04 19:53:58 +0900

    パスワード再設定フォーム作成

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :admin_accounts
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :admin_accounts, controllers: {
+     sessions: 'admin_accounts/sessions',
+     registrations: 'admin_accounts/registrations'
+  }
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   get 'sample', to: 'samples#index'
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
