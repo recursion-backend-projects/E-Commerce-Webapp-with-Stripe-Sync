881945f85dd49c4d30a78282cf7883801db3f0c9
AkinoJoey <gakiya213@gmail.com>
2024-05-06 20:05:44 +0900

    stockをnullableに変更、enumのstatusを追加

--- src/app/models/product.rb
+++ src/app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ApplicationRecord
+  enum :status, { draft: 0, published: 1, archived: 2, trashed: 3 }
 
   with_options presence: true do
     validates :name
     validates :price
-    validates :stock
     validates :description
   end
 end
