a11f754f6c14231167b12f0b294900187af12a95
seiichi0404 <yamayamaseisei1834@gmail.com>
2024-05-06 10:42:51 +0900

    ユーザー登録時のパラメーター許可設定

--- src/app/controllers/admin_accounts/registrations_controller.rb
+++ src/app/controllers/admin_accounts/registrations_controller.rb
@@ -41,9 +41,9 @@ class AdminAccounts::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
