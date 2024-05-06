b9a72371fbd410b167947cdf2971780ccecf0706
AkinoJoey <gakiya213@gmail.com>
2024-05-06 20:05:58 +0900

    status, stripe_product_idを追加

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_05_123328) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_06_102646) do
   create_table "articles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -21,10 +21,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_05_123328) do
   create_table "products", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.integer "price", null: false
-    t.integer "stock", default: 0, null: false
+    t.integer "stock", default: 0
     t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
+    t.string "stripe_product_id"
   end
 
 end
