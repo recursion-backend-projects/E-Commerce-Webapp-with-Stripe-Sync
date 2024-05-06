e39413ffb74406651cb3ecd53844136cf6686cc6
AkinoJoey <gakiya213@gmail.com>
2024-05-06 11:49:18 +0900

    product modelのバリデーションのテストを作成

---
+++ src/spec/models/product_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Product do
+  it 'is valid with a name, price, stock, description' do
+    expect(build(:product)).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    product = build(:product, name: nil)
+    product.valid?
+    expect(product.errors[:name]).to include("can't be blank")
+  end
+
+  it 'is invalid without a price' do
+    product = build(:product, price: nil)
+    product.valid?
+    expect(product.errors[:price]).to include("can't be blank")
+  end
+
+  it 'is invalid without a stock' do
+    product = build(:product, stock: nil)
+    product.valid?
+    expect(product.errors[:stock]).to include("can't be blank")
+  end
+
+  it 'is invalid without a description' do
+    product = build(:product, description: nil)
+    product.valid?
+    expect(product.errors[:description]).to include("can't be blank")
+  end
+end
