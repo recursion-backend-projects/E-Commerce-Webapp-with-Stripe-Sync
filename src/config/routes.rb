858f3a6299955e577513b01ede8714116a33ce7d
seiichi0404 <yamayamaseisei1834@gmail.com>
2024-05-07 11:24:16 +0900

    パスワードリセット機能追加

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :admin_accounts, controllers: {
      sessions: 'admin_accounts/sessions',
      registrations: 'admin_accounts/registrations',
-     confirmations: 'admin_accounts/confirmations'
+     confirmations: 'admin_accounts/confirmations',
+     passwords: 'admin_accounts/passwords'
   }
 
   get 'up' => 'rails/health#show', as: :rails_health_check
