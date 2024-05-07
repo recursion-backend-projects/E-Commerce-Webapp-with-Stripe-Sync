9fbcfa4246c1664e7b21394086698e05581bd3a8
seiichi0404 <yamayamaseisei1834@gmail.com>
2024-05-08 08:31:16 +0900

    セッションのタイムアウト設定

--- src/app/models/admin_account.rb
+++ src/app/models/admin_account.rb
@@ -2,5 +2,6 @@ class AdminAccount < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :timeoutable
 end
