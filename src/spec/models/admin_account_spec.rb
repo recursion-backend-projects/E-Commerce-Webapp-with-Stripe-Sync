255cdd6ed7701a79f8c1c896dabe86986e964edb
seiichi0404 <yamayamaseisei1834@gmail.com>
2024-05-08 23:32:12 +0900

    lint対応

--- src/spec/models/admin_account_spec.rb
+++ src/spec/models/admin_account_spec.rb
@@ -1,30 +1,50 @@
-# spec/models/admin_account_spec.rb
 require 'rails_helper'
 
 RSpec.describe AdminAccount, type: :model do
-  context 'validations' do
+  context 'when validating' do
     it 'is valid with valid attributes' do
       admin = build(:admin_account)
       expect(admin).to be_valid
     end
 
-    it 'requires an email' do
-      admin = build(:admin_account, email: nil)
-      expect(admin).not_to be_valid
-      expect(admin.errors[:email]).to include("を入力してください")
+    context 'when email is nil' do
+      it 'is not valid without an email' do
+        admin = build(:admin_account, email: nil)
+        expect(admin).not_to be_valid
+      end
+
+      it 'adds a correct error message for missing email' do
+        admin = build(:admin_account, email: nil)
+        expect(admin.errors[:email]).to include('を入力してください')
+      end
     end
 
-    it 'requires a password' do
-      admin = build(:admin_account, password: nil)
-      expect(admin).not_to be_valid
-      expect(admin.errors[:password]).to include("を入力してください")
+    context 'when password is nil' do
+      it 'is not valid without a password' do
+        admin = build(:admin_account, password: nil)
+        expect(admin).not_to be_valid
+      end
+
+      it 'adds a correct error message for missing password' do
+        admin = build(:admin_account, password: nil)
+        expect(admin.errors[:password]).to include('を入力してください')
+      end
     end
 
-    it 'has a unique email' do
-      create(:admin_account, email: "admin@example.com")
-      new_admin = build(:admin_account, email: "admin@example.com")
-      expect(new_admin).not_to be_valid
-      expect(new_admin.errors[:email]).to include("はすでに存在します")
+    context 'with a duplicate email' do
+      before do
+        create(:admin_account, email: 'admin@example.com')
+      end
+
+      it 'is not valid with a non-unique email' do
+        new_admin = build(:admin_account, email: 'admin@example.com')
+        expect(new_admin).not_to be_valid
+      end
+
+      it 'adds a correct error message for duplicate email' do
+        new_admin = build(:admin_account, email: 'admin@example.com')
+        expect(new_admin.errors[:email]).to include('はすでに存在します')
+      end
     end
   end
 end
