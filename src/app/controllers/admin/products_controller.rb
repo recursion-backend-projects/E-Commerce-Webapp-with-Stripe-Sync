e9a6b149c0ff72a2d60461103b5c4ba28c20113d
AkinoJoey <gakiya213@gmail.com>
2024-05-10 16:49:52 +0900

    edit, update, product_paramsアクションを作成

--- src/app/controllers/admin/products_controller.rb
+++ src/app/controllers/admin/products_controller.rb
@@ -1,11 +1,28 @@
 class Admin::ProductsController < ApplicationController
   def index
     @admin = true
+    @products = Product.all
   end
 
-  # def new
-  # end
+  def edit
+    @admin = true
+    @product = Product.find(params[:id])
+  end
 
-  # def edit
-  # end
+  def update
+    @admin = true
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params[:id])
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :description)
+  end
 end
