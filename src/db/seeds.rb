65e93440ac989daeee1d7fc88caf9625e472ef8e
AkinoJoey <gakiya213@gmail.com>
2024-05-12 19:22:03 +0900

    プロダクトカテゴリーの初期データを追加

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+#### Product Categories ####
+%w[絵画 彫刻 写真 イラスト].each do |product_category|
+  ProductCategory.find_or_create_by!(name: product_category)
+end
