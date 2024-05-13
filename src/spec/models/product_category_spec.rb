c034d9761048b8f0b392fa060b0ffc1d10326861
AkinoJoey <gakiya213@gmail.com>
2024-05-13 22:12:21 +0900

    ProductCategoryのテストケースを作成

--- src/spec/models/product_category_spec.rb
+++ src/spec/models/product_category_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe ProductCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with a name' do
+    expect(build(:product_category)).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    product_category = build(:product, name: nil)
+    product_category.valid?
+    expect(product_category.errors[:name]).to include('を入力してください')
+  end
 end
