16442c94d42a7a158e63ca443e536100d81e763e
seiichi0404 <yamayamaseisei1834@gmail.com>
2024-05-14 08:32:49 +0900

    ユニットテスト作成

--- src/spec/factories/customer_accounts.rb
+++ src/spec/factories/customer_accounts.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :customer_account do
-    
+    email { Faker::Internet.email }
+    password { 'password123' }
+    password_confirmation { 'password123' }
   end
 end
