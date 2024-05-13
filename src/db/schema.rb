e6cf28dbb995243105b8e9ddb6b4bfb30d19510c
AkinoJoey <gakiya213@gmail.com>
2024-05-13 22:11:45 +0900

    スキーマを更新

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -11,52 +11,59 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_05_12_101719) do
-    create_table "product_categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-      t.string "name"
-    end
+  create_table "addresses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "zip_code"
+    t.string "state"
+    t.string "city"
+    t.string "street_address"
+    t.string "street_address_2"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["city", "zip_code"], name: "index_addresses_on_city_and_zip_code"
+    t.index ["city"], name: "index_addresses_on_city"
+    t.index ["state"], name: "index_addresses_on_state"
+    t.index ["zip_code"], name: "index_addresses_on_zip_code"
+  end
 
-    create_table "addresses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-      t.integer "zip_code"
-      t.string "state"
-      t.string "city"
-      t.string "street_address"
-      t.string "street_address_2"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-      t.index ["city", "zip_code"], name: "index_addresses_on_city_and_zip_code"
-      t.index ["city"], name: "index_addresses_on_city"
-      t.index ["state"], name: "index_addresses_on_state"
-      t.index ["zip_code"], name: "index_addresses_on_zip_code"
-    end
+  create_table "admin_accounts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "user_name"
+    t.string "status"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_admin_accounts_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_admin_accounts_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_accounts_on_reset_password_token", unique: true
+  end
 
-    create_table "admin_accounts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-      t.string "email", default: "", null: false
-      t.string "encrypted_password", default: "", null: false
-      t.string "reset_password_token"
-      t.datetime "reset_password_sent_at"
-      t.datetime "remember_created_at"
-      t.string "user_name"
-      t.string "status"
-      t.string "confirmation_token"
-      t.datetime "confirmed_at"
-      t.datetime "confirmation_sent_at"
-      t.string "unconfirmed_email"
-    end
+  create_table "product_categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
-    create_table "products", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-      t.string "name", null: false
-      t.integer "price", null: false
-      t.integer "stock", default: 0
-      t.text "description", null: false
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-      t.integer "status", default: 0
-      t.string "stripe_product_id"
-      t.string "creator"
-      t.bigint "product_category_id"
-      t.string "stripe_price_id"
-      t.index ["product_category_id"], name: "index_products_on_product_category_id"
-    end
+  create_table "products", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.integer "stock", default: 0
+    t.text "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "status", default: 0
+    t.string "stripe_product_id"
+    t.string "creator"
+    t.bigint "product_category_id"
+    t.string "stripe_price_id"
+    t.index ["product_category_id"], name: "index_products_on_product_category_id"
+  end
 
-    add_foreign_key "products", "product_categories"
+  add_foreign_key "products", "product_categories"
 end
