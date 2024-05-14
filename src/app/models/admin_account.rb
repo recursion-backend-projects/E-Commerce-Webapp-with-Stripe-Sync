8e4ff83ff3c765a53d4159ef64a613fbba7094b1
seiichi0404 <yamayamaseisei1834@gmail.com>
2024-05-14 09:37:11 +0900

    不要な機能を削除

--- src/app/models/admin_account.rb
+++ src/app/models/admin_account.rb
@@ -1,9 +1,8 @@
 class AdminAccount < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable, :timeoutable
+  devise :database_authenticatable, :timeoutable,
+         :rememberable, :validatable
 
   validates :password, format: {
     with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
