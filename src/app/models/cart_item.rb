e9830c322a2d13e3b7920e4bf8928f9b287d582d
AkinoJoey <gakiya213@gmail.com>
2024-05-16 17:52:18 +0900

    ini関数とsubtotal関数を追加

--- src/app/models/cart_item.rb
+++ src/app/models/cart_item.rb
@@ -1,3 +1,13 @@
 class CartItem < ApplicationRecord
-  belongs_to :product
+  attr_reader :product_id, :quantity
+
+  def initialize(product_id, quantity)
+    super
+    @product_id = product_id
+    @quantity = quantity
+  end
+
+  def subtotal
+    # ここで商品の価格と数量を計算して小計を返す
+  end
 end
