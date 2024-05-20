1b8963d4edd64d114321532366f170f33d9cc771
AkinoJoey <gakiya213@gmail.com>
2024-05-20 15:50:52 +0900

    current_cartとcart_items_countメソッドを追加

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :current_cart
+  before_action :cart_items_count
+
+  private
+
+  # 現在のカートを取得するメソッド
+  def current_cart
+    # @current_cartが未定義またはnilの場合、session[:cart]を使用し、それも未定義またはnilの場合は空のハッシュを設定する
+    @current_cart ||= session[:cart] ||= {}
+  end
+
+  def cart_items_count
+    @cart_items_count = session[:cart_items_count] ||= @current_cart.values.sum
+  end
 end
