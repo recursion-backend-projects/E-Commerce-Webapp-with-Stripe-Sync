3eab942f1c111ba3498431f6c64dadccd61e95cc
AkinoJoey <gakiya213@gmail.com>
2024-05-20 15:51:26 +0900

    showメソッドを修正

--- src/app/controllers/customer/products_controller.rb
+++ src/app/controllers/customer/products_controller.rb
@@ -2,8 +2,7 @@ class Customer::ProductsController < ApplicationController
   def show
     @customer = true
     @product = Product.find(params[:id])
-    @cart = session[:cart] || {}
-    @product_count_in_cart = @cart&.dig(@product.id.to_s) || 0
+    @product_count_in_cart = @current_cart&.dig(@product.id.to_s) || 0
     @remaining_stock = @product.stock - @product_count_in_cart
   end
 end
