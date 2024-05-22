28f92e022ee25a5f687f6be6bd96abd56f10e0bf
Aki158 <xiaolinh548@gmail.com>
2024-05-23 00:28:56 +0900

    JavaScriptとCSSをビルドするように変更

--- src/bin/docker-prod-entrypoint.sh
+++ src/bin/docker-prod-entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# JavaScriptのビルド
+yarn build
+
+# CSSのビルド
+yarn build:css
+
 # アセットプリコンパイルの実行(https://github.com/rails/rails/pull/46760)
 bundle exec rake assets:precompile SECRET_KEY_BASE_DUMMY=1 RAILS_ENV=production
 
