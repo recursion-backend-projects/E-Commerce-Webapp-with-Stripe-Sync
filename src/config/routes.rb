d5314bd28e8871ca96672dbaa6d158aa69cb6b59
seiichi0404 <yamayamaseisei1834@gmail.com>
2024-05-24 22:25:29 +0900

    homeは別コントローラーで管理

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
   # アカウント認証(カスタマー)
   devise_for :customer_accounts, controllers: {
     sessions: 'customer_accounts/sessions',
@@ -26,7 +28,7 @@ Rails.application.routes.draw do
   end
   # カスタマーのルーティング
   scope module: :customer do
-    resources :products, only: %i[show index]
+    resources :products, only: %i[show]
     resource :cart, only: %i[show create update destroy]
     resources :customer_accounts, only: [] do
       resources :wish_products, only: %i[index create destroy]
