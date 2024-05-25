0e63ce9199bb4ad383f2c63e3c3b364e752c1ce2
seiichi0404 <yamayamaseisei1834@gmail.com>
2024-05-25 16:20:30 +0900

    lint

--- src/app/controllers/customer/favorite_products_controller.rb
+++ src/app/controllers/customer/favorite_products_controller.rb
@@ -11,9 +11,9 @@ class Customer::FavoriteProductsController < ApplicationController
     favorite_product = current_customer_account.customer.favorite_products.new(product:)
 
     if favorite_product.save
-        redirect_to product_path(product.id)
+      redirect_to product_path(product.id)
     else
-        redirect_to products_path, alert: 'Failed to add product to favorites.'
+      redirect_to products_path, alert: 'Failed to add product to favorites.'
     end
   end
 
@@ -21,8 +21,8 @@ class Customer::FavoriteProductsController < ApplicationController
     favorite_product = current_customer_account.customer.favorite_products.find(params[:id])
     favorite_product.destroy
 
-    redirect_to favorite_products_path(), notice: 'Product removed from favorites.'
+    redirect_to favorite_products_path, notice: 'Product removed from favorites.'
   rescue ActiveRecord::RecordNotFound
-    redirect_to favorite_products_path(), alert: 'Favorite product not found.'
+    redirect_to favorite_products_path, alert: 'Favorite product not found.'
   end
 end
