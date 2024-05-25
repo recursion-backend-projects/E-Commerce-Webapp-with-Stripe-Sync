28f7c4a77cf026a42539c2fc6f16ac3c024104f3
seiichi0404 <yamayamaseisei1834@gmail.com>
2024-05-25 11:50:12 +0900

    テーブル設計を修正

--- src/app/models/customer.rb
+++ src/app/models/customer.rb
@@ -1,14 +1,6 @@
 class Customer < ApplicationRecord
-  belongs_to :customer_account, inverse_of: :customer
-  has_one :address, as: :addressable, dependent: :destroy, inverse_of: :addressable
   has_many :wish_products, dependent: :destroy
   has_many :favorite_products, dependent: :destroy
-
-  before_save :set_address
-
-  private
-
-  def set_address
-    self.address_id ||= address.id if address
-  end
+  has_one :customer_account, dependent: :destroy
+  has_one :address, as: :addressable, dependent: :destroy, inverse_of: :addressable
 end
