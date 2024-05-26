8e7b09b637677cbcb9922c50d474ceba7807d728
AkinoJoey <gakiya213@gmail.com>
2024-05-26 18:58:58 +0900

    Eメール認証後にstripeのCustomerオブジェクトを作成するようにした

--- src/app/models/customer_account.rb
+++ src/app/models/customer_account.rb
@@ -10,4 +10,20 @@ class CustomerAccount < ApplicationRecord
   }, on: :create
 
   belongs_to :customer
+
+  # Eメール認証後に実行するdeviseの関数をオーバーライド
+  def after_confirmation
+    create_stripe_customer
+  end
+
+  private
+
+  def create_stripe_customer
+    stripe_customer = Stripe::Customer.create({
+                                                name: user_name,
+                                                email:
+                                              })
+    customer = Customer.find_by(id: customer_id)
+    customer.update(stripe_customer_id: stripe_customer.id)
+  end
 end
