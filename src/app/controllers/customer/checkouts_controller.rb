58ab680d0b3b7bb40b9f20792e01597d74e5c4bb
AkinoJoey <gakiya213@gmail.com>
2024-05-26 15:51:03 +0900

    show関数が不要になったので削除

--- src/app/controllers/customer/checkouts_controller.rb
+++ src/app/controllers/customer/checkouts_controller.rb
@@ -1,56 +1,37 @@
 class Customer::CheckoutsController < ApplicationController
-  def show
-    @customer = true
-    cart = current_cart
-
-    if cart.empty?
-      redirect_to cart_path
-    else
-      @total = 0
-      cart.each_key do |product_id|
-        product = Product.find_by(id: product_id.to_i)
-        if product.nil? || product.stock <= 0
-          # 商品が削除されている場合はカートにリダイレクトする
-          redirect_to cart_path
-        else
-          @total += product.price * session[:cart][product_id]
-        end
-      end
-      # PaymentIntentを作成
-      # https://docs.stripe.com/api/payment_intents
-      @intent = Stripe::PaymentIntent.create(
-        amount: calculate_order_amount,
-        currency: 'jpy'
-      )
-    end
+  def create
+    line_items = generate_line_items
+    session = Stripe::Checkout::Session.create({
+                                                 line_items:,
+                                                 mode: 'payment',
+                                                 #  ユーザーがCheckoutで戻るボタンをクリックするとこのページにリダイレクトされるURL
+                                                 cancel_url: cart_url,
+                                                 #  決済が完了したときにリダイレクトされるURL
+                                                 success_url: 'http://localhost:3000/checkout/success?session_id={CHECKOUT_SESSION_ID}',
+                                                 #  TODO:デジタル商品のみの場合はshipping_address_collectionを不要なので削除する
+                                                 shipping_address_collection: {
+                                                   allowed_countries: %w[JP]
+                                                 },
+                                                 billing_address_collection: 'required'
+                                               })
+    redirect_to session.url, allow_other_host: true
   end
-
-  # def create
-  #   line_items = generate_line_items
-  #   session = Stripe::Checkout::Session.create({
-  #                                                line_items:,
-  #                                                mode: 'payment',
-  #                                                #  ユーザーがCheckoutで戻るボタンをクリックするとこのページにリダイレクトされるURL
-  #                                                cancel_url: cart_url,
-  #                                                #  決済が完了したときにリダイレクトされるURL
-  #                                                success_url: 'http://localhost:3000/checkout/success?session_id={CHECKOUT_SESSION_ID}'
-  #                                              })
-  #   redirect_to session.url, allow_other_host: true
-  # end
 
   # 支払いが成功した時に表示するページを表示するアクション
   def success
     @customer = true
 
     # 有効なsession_idのクエリじゃない場合はアラートを出す
-    # begin
-    #   Stripe::Checkout::Session.retrieve(params[:session_id])
-    # rescue Stripe::InvalidRequestError => e
-    #   logger.error(e)
-    #   # TODO: root_pathに変更する
-    #   redirect_to cart_path
-    #   flash[:alert] = '無効なページです'
-    # end
+    begin
+      Stripe::Checkout::Session.retrieve(params[:session_id])
+      # 決済が完了したのでカートをリセットする
+      reset_cart
+    rescue Stripe::InvalidRequestError => e
+      logger.error(e)
+      # TODO: root_pathに変更する
+      redirect_to cart_path
+      flash[:alert] = '無効なページです'
+    end
   end
 
   private
@@ -72,9 +53,5 @@ class Customer::CheckoutsController < ApplicationController
     end
 
     line_items
-  end
-
-  def calculate_order_amount
-    1099
   end
 end
