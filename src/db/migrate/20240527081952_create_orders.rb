47c7cb8e5bc6835928f04a8eef6b0a70a99a13dd
AkinoJoey <gakiya213@gmail.com>
2024-05-28 17:53:48 +0900

    receipt_urlを追加

--- src/db/migrate/20240527081952_create_orders.rb
+++ src/db/migrate/20240527081952_create_orders.rb
@@ -1,11 +1,12 @@
 class CreateOrders < ActiveRecord::Migration[7.1]
   def change
     create_table :orders do |t|
-      t.string :order_number
+      t.string :order_number, index: { unique: true}
       t.integer :total
       t.datetime :order_date
       t.string :guest_email
-      t.references :customer, null: false, foreign_key: true
+      t.string :receipt_url
+      t.references :customer, null: true, foreign_key: true
 
       t.timestamps
     end
