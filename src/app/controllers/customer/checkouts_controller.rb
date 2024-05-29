47b17ecc2237edf716ed7ab3300d8fada780b356
AkinoJoey <gakiya213@gmail.com>
2024-05-29 17:47:54 +0900

    rubocop

--- src/app/controllers/customer/checkouts_controller.rb
+++ src/app/controllers/customer/checkouts_controller.rb
@@ -17,7 +17,7 @@ class Customer::CheckoutsController < ApplicationController
                                                    allowed_countries: %w[JP]
                                                  },
                                                  # requiredにしないと、請求先は国だけ選べるという微妙な見た目になってしまう
-                                                 billing_address_collection: 'required',
+                                                 billing_address_collection: 'required'
                                                })
     redirect_to session.url, allow_other_host: true
   end
