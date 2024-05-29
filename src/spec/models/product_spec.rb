dcf06d6cb8d9322efeee3ee2440ffe1be6ff33be
seiichi0404 <yamayamaseisei1834@gmail.com>
2024-05-29 11:10:26 +0900

    ユニットテスト追加

--- src/spec/models/product_spec.rb
+++ src/spec/models/product_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Product do
   let(:product) { create(:product) }
+  let(:valid_image) { fixture_file_upload(Rails.root.join('spec/fixtures/files/valid_image.jpg'), 'image/jpeg') }
+  let(:invalid_type_image) { fixture_file_upload(Rails.root.join('spec/fixtures/files/invalid_image.txt'), 'text/plain') }
+  let(:invalid_size_image) { fixture_file_upload(Rails.root.join('spec/fixtures/files/large_image.jpg'), 'image/jpeg') }
 
   it 'is valid with a name, price, description, product_category_id' do
     expect(product).to be_valid
@@ -23,5 +26,24 @@ RSpec.describe Product do
     product.description = nil
     product.valid?
     expect(product.errors[:description]).to include('を入力してください')
+  end
+
+  context 'with image validation' do
+    it 'is valid with a valid image' do
+      product.images.attach(valid_image)
+      expect(product).to be_valid
+    end
+
+    it 'is invalid with a non-image file' do
+      product.images.attach(invalid_type_image)
+      product.valid?
+      expect(product.errors[:images]).to include('のContent Typeが不正です')
+    end
+
+    it 'is invalid with an image larger than 3MB' do
+      product.images.attach(invalid_size_image)
+      product.valid?
+      expect(product.errors[:images]).to include('ファイル サイズは 3MB 未満にする必要があります (現在のサイズは 3.11MB)')
+    end
   end
 end
