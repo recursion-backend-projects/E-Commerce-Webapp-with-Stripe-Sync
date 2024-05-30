da61c423b15805717d193a221518e7b6b7077d0e
Aki158 <xiaolinh548@gmail.com>
2024-05-30 16:12:53 +0900

    商品が見つからない場合のリダイレクト追加

--- src/app/controllers/customer/products_controller.rb
+++ src/app/controllers/customer/products_controller.rb
@@ -1,10 +1,18 @@
 class Customer::ProductsController < ApplicationController
+  before_action :set_product, only: %i[show]
+
   def show
     @customer = true
-    @product = Product.find(params[:id])
     @product_reviews = @product.product_reviews.order(created_at: :desc).includes(customer: :customer_account).limit(5)
     @average_rating = @product.product_reviews.average(:rating).to_i
     @product_count_in_cart = @current_cart&.dig(@product.id.to_s) || 0
     @remaining_stock = @product.stock - @product_count_in_cart
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = '商品が見つかりません'
+    redirect_to root_path
   end
 end
