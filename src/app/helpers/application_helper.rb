426a3314fefc3f102610bca43fca294a803c924f
Aki158 <xiaolinh548@gmail.com>
2024-05-30 17:49:43 +0900

    改行削除

--- src/app/helpers/application_helper.rb
+++ src/app/helpers/application_helper.rb
@@ -41,7 +41,6 @@ module ApplicationHelper
 
   def star_rating(rating)
     rating = 0 if rating.nil?
-
     full_stars = '★' * rating
     empty_stars = '☆' * (5 - rating)
     full_stars + empty_stars
