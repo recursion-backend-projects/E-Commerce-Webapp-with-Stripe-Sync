8137541f965f9218d96ec1c4ecdf7b9637261f29
Aki158 <xiaolinh548@gmail.com>
2024-05-30 17:07:35 +0900

    rubocopのルールに違反するコード修正

--- src/spec/models/product_review_spec.rb
+++ src/spec/models/product_review_spec.rb
@@ -24,32 +24,37 @@ RSpec.describe ProductReview, type: :model do
     it 'is invalid without a title' do
       product_review.title = nil
       product_review.valid?
-      expect(product_review.errors[:title]).to include("を入力してください")
+      expect(product_review.errors[:title]).to include('を入力してください')
     end
 
     it 'is invalid without a review' do
       product_review.review = nil
       product_review.valid?
-      expect(product_review.errors[:review]).to include("を入力してください")
+      expect(product_review.errors[:review]).to include('を入力してください')
     end
 
     it 'is invalid without a rating' do
       product_review.rating = nil
       product_review.valid?
-      expect(product_review.errors[:rating]).to include("を入力してください")
-      expect(product_review.errors[:rating]).to include("は一覧にありません")
+      expect(product_review.errors[:rating]).to include('を入力してください')
+    end
+
+    it 'rating must be included in the list' do
+      product_review.rating = nil
+      product_review.valid?
+      expect(product_review.errors[:rating]).to include('は一覧にありません')
     end
 
     it 'is invalid with a title longer than 50 characters' do
       product_review.title = 'a' * 51
       product_review.valid?
-      expect(product_review.errors[:title]).to include("は50文字以内で入力してください")
+      expect(product_review.errors[:title]).to include('は50文字以内で入力してください')
     end
 
     it 'is invalid with a review longer than 400 characters' do
       product_review.review = 'a' * 401
       product_review.valid?
-      expect(product_review.errors[:review]).to include("は400文字以内で入力してください")
+      expect(product_review.errors[:review]).to include('は400文字以内で入力してください')
     end
   end
 end
