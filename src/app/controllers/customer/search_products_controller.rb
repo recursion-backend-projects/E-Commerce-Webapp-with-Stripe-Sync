91a24a2ffeefffec3d49134f04842236c1e8c6c3
Aki158 <xiaolinh548@gmail.com>
2024-06-01 16:44:50 +0900

    ホームページとナビバーの商品リストページへのリンク作成

--- src/app/controllers/customer/search_products_controller.rb
+++ src/app/controllers/customer/search_products_controller.rb
@@ -1,5 +1,24 @@
 class Customer::SearchProductsController < ApplicationController
+  # 許可するカラム名を指定
+  VALID_COLUMNS = ["name", "description", "category", "creator"]
+
   def index
     @customer = true
+
+    if params[:type].present? && params[:keyword].present? && VALID_COLUMNS.include?(params[:type])
+      if params[:type] == "category"
+        @products = Product.joins(:product_category).where(product_category: { name: "#{params[:keyword]}" }) 
+      else
+        @products = Product.where("#{params[:type]} LIKE ?", "%#{params[:keyword]}%")
+      end
+    else
+      @products = Product.all
+    end
   end
+
+  # def set_product
+  #   @product = Product.find(params[:product_id])
+  # rescue ActiveRecord::RecordNotFound
+  #   redirect_to root_path, alert: '商品が見つかりません'
+  # end
 end
