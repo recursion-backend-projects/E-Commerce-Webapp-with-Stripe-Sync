733206f8a66b5c39c7945cef450119ae7e3ecafb
AkinoJoey <gakiya213@gmail.com>
2024-06-03 11:06:00 +0900

    shippingsの並び順をdescに変更した

--- src/app/controllers/admin/shippings_controller.rb
+++ src/app/controllers/admin/shippings_controller.rb
@@ -1,7 +1,7 @@
 class Admin::ShippingsController < ApplicationController
   def index
     @admin = true
-    @shippings = Shipping.all
+    @shippings = Shipping.all.order(created_at: :desc)
   end
 
   def edit
