de4c52ad5612665f5ed69c930727f68e35c791ee
AkinoJoey <gakiya213@gmail.com>
2024-06-04 17:50:02 +0900

    addressのバリデーションを作成

--- src/app/models/address.rb
+++ src/app/models/address.rb
@@ -3,4 +3,11 @@ class Address < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
+
+  with_options allow_blank: true do
+    validates :zip_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'の形式が不正です（例:123-4567）' }
+    validates :prefecture_id, numericality: { only_integer: true, in: 1..47 }
+    validates :street_address, length: { maximum: 200 }
+    validates :street_address_2, length: { maximum: 200 }
+  end
 end
