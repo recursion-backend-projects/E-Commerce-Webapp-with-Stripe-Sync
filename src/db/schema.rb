5d07ec72292c05a487235e5fb707fc7a4f69d1c4
AkinoJoey <gakiya213@gmail.com>
2024-06-04 17:51:52 +0900

    スキーマを更新

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_03_081220) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_04_084709) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -40,7 +40,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_03_081220) do
   end
 
   create_table "addresses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.integer "zip_code"
+    t.string "zip_code"
     t.string "street_address"
     t.string "street_address_2"
     t.datetime "created_at", null: false
