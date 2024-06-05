cd90e34f95d3d8138aa892987a02c14ee0fce101
AkinoJoey <gakiya213@gmail.com>
2024-06-05 19:21:55 +0900

    不要なので削除

--- src/spec/factories/addresses.rb
+++ src/spec/factories/addresses.rb
@@ -1,10 +1,5 @@
 FactoryBot.define do
   factory :address do
-    zip_code { '1234567' }
-    state { 'Tokyo' }
-    city { 'Chiyoda' }
-    street_address { '1-1-1' }
-    street_address_2 { 'Building 101' }
     addressable { association :customer }
   end
 end
