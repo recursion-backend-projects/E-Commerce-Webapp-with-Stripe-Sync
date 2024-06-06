9eb3a3d4af8094bd52d35efec2ff29097bc53bd2
AkinoJoey <gakiya213@gmail.com>
2024-06-06 17:52:36 +0900

    tagInputを作成途中

--- src/app/javascript/product_form.ts
+++ src/app/javascript/product_form.ts
@@ -1,22 +1,30 @@
+import Tagify from "@yaireo/tagify";
+
 document.addEventListener("DOMContentLoaded", function () {
-  const productTypeSelect = document.getElementById(
-    "product_product_type"
-  ) as HTMLSelectElement;
-  const digitalFileField = document.getElementById(
-    "digital_file_field"
-  ) as HTMLElement;
+	const productTypeSelect = document.getElementById(
+		"product_product_type"
+	) as HTMLSelectElement;
+	const digitalFileField = document.getElementById(
+		"digital_file_field"
+	) as HTMLElement;
 
-  function toggleDigitalFileField() {
-    if (productTypeSelect.value === "digital") {
-      // 'digital'を正しい値に置き換える必要があります
-      digitalFileField.style.display = "block";
-    } else {
-      digitalFileField.style.display = "none";
-    }
-  }
+	const tagInput = document.getElementById(
+		"product_tag_list"
+	) as HTMLInputElement;
 
-  productTypeSelect.addEventListener("change", toggleDigitalFileField);
+new Tagify(tagInput);
 
-  // 初期状態の設定
-  toggleDigitalFileField();
+	function toggleDigitalFileField() {
+		if (productTypeSelect.value === "digital") {
+			// 'digital'を正しい値に置き換える必要があります
+			digitalFileField.style.display = "block";
+		} else {
+			digitalFileField.style.display = "none";
+		}
+	}
+
+	productTypeSelect.addEventListener("change", toggleDigitalFileField);
+
+	// 初期状態の設定
+	toggleDigitalFileField();
 });
