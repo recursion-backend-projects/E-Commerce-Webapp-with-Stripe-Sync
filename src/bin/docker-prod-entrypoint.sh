1811fa68f09c0dd770a0e455d60c9c10acb01a8c
seiichi0404 <yamayamaseisei1834@gmail.com>
2024-06-06 21:39:50 +0900

    スクリプト変更

--- src/bin/docker-prod-entrypoint.sh
+++ src/bin/docker-prod-entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-echo "Starting docker-prod-entrypoint.sh"
+echo "Starting docker-prod-entrypoint.sh2"
 
 mkdir -p /E-Commerce-Webapp-with-Stripe-Sync/tmp/pids /E-Commerce-Webapp-with-Stripe-Sync/tmp/sockets
 
-bundle exec db:prepare
+# 必要なgemをインストール
+bundle install
+
+# データベースの準備
+bundle exec rails db:prepare
 
 # アセットプリコンパイルの実行(https://github.com/rails/rails/pull/46760)
 bundle exec rake assets:precompile SECRET_KEY_BASE_DUMMY=1 RAILS_ENV=production
