56826d4239acfd59852d59b93cff4fe3af9e41c0
AkinoJoey <gakiya213@gmail.com>
2024-06-07 17:40:21 +0900

    タグ補完のためのfetchを作成

--- src/app/javascript/product_form.ts
+++ src/app/javascript/product_form.ts
@@ -12,7 +12,45 @@ document.addEventListener("DOMContentLoaded", function () {
 		"product_tag_list"
 	) as HTMLInputElement;
 
-new Tagify(tagInput);
+	const tagify = new Tagify(tagInput, {
+		originalInputValueFormat: (valuesArr: { value: string }[]) =>
+			valuesArr.map((item: { value: string }) => item.value).join(","),
+		whitelist: [],
+	});
+
+	let controller: AbortController | undefined;
+
+	tagify.on("input", onTagInput);
+
+	function onTagInput(e: CustomEvent): void {
+		let value = e.detail.value;
+		tagify.whitelist = null;
+
+		// 以前のリクエストを中止
+		if (controller) {
+			controller.abort();
+		}
+
+		// 新しいAbortControllerを作成
+		controller = new AbortController();
+
+		tagify.loading(true);
+
+		fetch(`/admin/products/tags/whitelist?name=${value}`, {
+			signal: controller.signal,
+		})
+			.then((response) => response.json())
+			.then((newWhitelist: string[]) => {
+				// ホワイトリストを更新
+				tagify.whitelist = newWhitelist;
+
+				// ローディングアニメーションを非表示にしてドロップダウンを表示
+				tagify.loading(false).dropdown.show(value);
+			})
+			.catch((error) => {
+				console.error("エラーが発生しました", error);
+			});
+	}
 
 	function toggleDigitalFileField() {
 		if (productTypeSelect.value === "digital") {
