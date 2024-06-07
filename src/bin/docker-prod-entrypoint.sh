0d015069442feea1f4eba2374e3b570ac8bf7a37
seiichi0404 <yamayamaseisei1834@gmail.com>
2024-06-07 16:13:08 +0900

    ビルド処理を追加

--- src/bin/docker-prod-entrypoint.sh
+++ src/bin/docker-prod-entrypoint.sh
@@ -10,6 +10,12 @@ bundle install
 # データベースの準備
 bundle exec rails db:prepare
 
+# JavaScriptのビルド
+yarn build
+
+# CSSのビルド
+yarn build:css
+
 # アセットプリコンパイルの実行(https://github.com/rails/rails/pull/46760)
 bundle exec rake assets:precompile SECRET_KEY_BASE_DUMMY=1 RAILS_ENV=production
 
