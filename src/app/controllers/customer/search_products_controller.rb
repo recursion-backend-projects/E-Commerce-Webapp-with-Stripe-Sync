8a08d18d605e1b4ae8b3064fef132956da1cdb55
Aki158 <xiaolinh548@gmail.com>
2024-06-08 17:34:17 +0900

    未入力検索時の挙動修正と全角スペースの削除処理追加

--- src/app/controllers/customer/search_products_controller.rb
+++ src/app/controllers/customer/search_products_controller.rb
@@ -1,9 +1,16 @@
 class Customer::SearchProductsController < ApplicationController
   def index
     @customer = true
+    # デフォルトの並び替えを高評価順で設定する
     @search.sorts = 'average_rating desc' if @search.sorts.empty?
-    @keyword = params[:q]['name_or_description_or_creator_or_product_category_name_cont'] || params[:q]['product_category_name_eq']
-    @products = @keyword.blank? ? nil : @search.result(distinct: true)
+    # 不要な全角空白があれば、削除する
+    if @search.name_or_description_or_creator_or_product_category_name_cont.present?
+      @search.name_or_description_or_creator_or_product_category_name_cont =
+        @search.name_or_description_or_creator_or_product_category_name_cont.gsub(/　/, ' ').strip
+    end
+    @keyword = @search.name_or_description_or_creator_or_product_category_name_cont ||
+               @search.product_category_name_eq
+    @products = @keyword.blank? ? Product.all : @search.result(distinct: true)
     @average_ratings = {}
 
     return if @products.blank?
