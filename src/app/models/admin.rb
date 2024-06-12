f03e88450ab7f16d4cb4546b3873842bed427e48
AkinoJoey <gakiya213@gmail.com>
2024-06-12 19:11:52 +0900

    has_oneをcustomer_accountからadmin_accountに修正

--- src/app/models/admin.rb
+++ src/app/models/admin.rb
@@ -1,4 +1,4 @@
 class Admin < ApplicationRecord
-  belongs_to :customer_account
+  has_one :admin_account, dependent: :destroy
   has_one :address, as: :addressable, dependent: :destroy, inverse_of: :addressable
 end
