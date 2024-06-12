c96ba73d63308674c0ec1be957db85120482028f
AkinoJoey <gakiya213@gmail.com>
2024-06-12 19:11:02 +0900

    chat変数を追加

--- src/app/controllers/admin/chats_controller.rb
+++ src/app/controllers/admin/chats_controller.rb
@@ -1,9 +1,11 @@
 class Admin::ChatsController < ApplicationController
   def index
     @admin = true
+    @chats = Chat.where(status: :waiting_for_admin).or(Chat.where(status: :chatting))
   end
 
   def show
     @admin = true
+    @chat = Chat.find(params[:id])
   end
 end
