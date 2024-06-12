6c1b1ec2985c9f3c3759901a9524b87b975104ad
AkinoJoey <gakiya213@gmail.com>
2024-06-12 19:12:13 +0900

    generate_jwt, decode_jwt関数を作成

--- src/app/models/customer.rb
+++ src/app/models/customer.rb
@@ -6,7 +6,21 @@ class Customer < ApplicationRecord
   has_one :customer_account, dependent: :destroy
   has_one :address, as: :addressable, dependent: :destroy, inverse_of: :addressable
   has_many :orders, dependent: :destroy
+  has_one :chat, dependent: :destroy
 
   accepts_nested_attributes_for :customer_account
   accepts_nested_attributes_for :address
+
+  def generate_jwt
+    hmac_secret = ENV.fetch('JWT_SECRET_KEY')
+    payload = { type: 'create', customer_id: id, exp: 8.hours.from_now.to_i }
+    JWT.encode(payload, hmac_secret, 'HS256')
+  end
+
+  def self.decode_jwt(token)
+    decoded = JWT.decode(token, ENV.fetch('JWT_SECRET_KEY'), true, { algorithm: 'HS256' })
+    decoded[0] # デコードしたペイロードを返す
+  rescue JWT::ExpiredSignature, JWT::DecodeError
+    nil
+  end
 end
