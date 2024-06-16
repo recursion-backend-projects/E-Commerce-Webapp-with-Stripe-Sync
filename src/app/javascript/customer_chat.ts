714362b017178c5ca04a1c6d7097f0a4f870c3c8
seiichi0404 <yamayamaseisei1834@gmail.com>
2024-06-16 11:23:43 +0900

    インデント修正

--- src/app/javascript/customer_chat.ts
+++ src/app/javascript/customer_chat.ts
@@ -1,50 +1,50 @@
 document.addEventListener("DOMContentLoaded", async function () {
-	async function getToken() {
-		let res = await fetch("/chat/token");
-		let data = await res.json();
-		return data.token;
-	}
+  async function getToken() {
+    let res = await fetch("/chat/token");
+    let data = await res.json();
+    return data.token;
+  }
 
-    let token = await getToken();
-    console.log(`token: ${token}`);
-    
-	const websocketUrl = document
-		.getElementById("websocket-url")
-		?.getAttribute("data-websocket-url");
+  let token = await getToken();
+  console.log(`token: ${token}`);
 
-    const socket = new WebSocket(`${websocketUrl}?token=${token}`);
+  const websocketUrl = document
+    .getElementById("websocket-url")
+    ?.getAttribute("data-websocket-url");
 
-	socket.onopen = function (event) {
-        console.log("Connected to WebSocket server.");
-	};
+  const socket = new WebSocket(`${websocketUrl}?token=${token}`);
 
-	socket.onclose = function (event) {
-		console.log("Disconnected from WebSocket server.");
-    };
-    
-    socket.onmessage = function (event) {
-        console.log(`server: ${event.data}`);
+  socket.onopen = function (event) {
+    console.log("Connected to WebSocket server.");
+  };
+
+  socket.onclose = function (event) {
+    console.log("Disconnected from WebSocket server.");
+  };
+
+  socket.onmessage = function (event) {
+    console.log(`server: ${event.data}`);
+  };
+
+  document.getElementById("submit")!.addEventListener("click", function (e) {
+    e.preventDefault();
+    sendMessage();
+  });
+
+  const chatInput = document.getElementById("chat") as HTMLInputElement;
+  chatInput?.focus();
+
+  chatInput?.addEventListener("keydown", function (e) {
+    if (e.key === "Enter" && e.shiftKey) {
+      e.preventDefault();
+      sendMessage();
     }
+  });
 
-	document.getElementById("submit")!.addEventListener("click", function (e) {
-		e.preventDefault();
-		sendMessage();
-    });
-
-	const chatInput = document.getElementById("chat") as HTMLInputElement;
-    chatInput?.focus();
-    
-    chatInput?.addEventListener("keydown", function (e) {
-			if (e.key === "Enter" && e.shiftKey) {
-				e.preventDefault();
-				sendMessage();
-			}
-		});
-
-	function sendMessage() {
-		const message = chatInput?.value;
-        socket.send(message);
-        console.log(`me: ${message}`)
-        chatInput.value = "";
-	}
+  function sendMessage() {
+    const message = chatInput?.value;
+    socket.send(message);
+    console.log(`me: ${message}`);
+    chatInput.value = "";
+  }
 });
