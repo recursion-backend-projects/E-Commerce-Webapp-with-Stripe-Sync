aeb0fce2a5420c40320db83e6960fac56c064089
AkinoJoey <gakiya213@gmail.com>
2024-06-16 19:26:57 +0900

    レジに進むボタンからの決済とご購入手続へからの決済方法の両方に対応できるようにした

--- src/app/controllers/customer/checkouts_controller.rb
+++ src/app/controllers/customer/checkouts_controller.rb
@@ -1,25 +1,23 @@
 class Customer::CheckoutsController < ApplicationController
   def create
-    line_items = generate_line_items
-    customer = current_customer
+    # 「ご購入手続きへ」からの購入の場合
+    if params[:checkout_action]
+      product_id = params[:product_id]
+      quantity = params[:quantity].to_i
+      line_items = generate_line_items({ product_id => quantity })
+      success_url = generate_success_url('direct')
 
-    session = Stripe::Checkout::Session.create({
-                                                 client_reference_id: customer.present? ? customer.id : nil,
-                                                 customer: customer.present? ? customer&.stripe_customer_id : nil,
-                                                 line_items:,
-                                                 mode: 'payment',
-                                                 #  ユーザーがCheckoutで戻るボタンをクリックするとこのページにリダイレクトされるURL
-                                                 cancel_url: cart_url,
-                                                 #  決済が完了したときにリダイレクトされるURL
-                                                 success_url:,
-                                                 #  TODO:デジタル商品のみの場合はshipping_address_collectionを不要なので削除する
-                                                 shipping_address_collection: {
-                                                   allowed_countries: %w[JP]
-                                                 },
-                                                 # requiredにしないと、請求先は国だけ選べるという微妙な見た目になってしまう
-                                                 billing_address_collection: 'required'
-                                               })
-    redirect_to session.url, allow_other_host: true
+    # 「レジに進む」からの購入の場合
+    elsif params[:register_action]
+      line_items = generate_line_items(current_cart)
+      success_url = generate_success_url('register')
+    else
+      redirect_back fallback_location: root_path, alert: '不明なアクションです'
+    end
+
+    customer = current_customer
+    session = create_checkout_session(customer, line_items, success_url)
+    redirect_to session.url, status: :see_other, allow_other_host: true
   end
 
   # 支払いが成功した時に表示するページを表示するアクション
@@ -29,22 +27,24 @@ class Customer::CheckoutsController < ApplicationController
     # 有効なsession_idのクエリじゃない場合はアラートを出す
     begin
       Stripe::Checkout::Session.retrieve(params[:session_id])
-      # 決済が完了したのでカートをリセットする
-      reset_cart
+
+      # 「レジに進む」からの購入の場合のみ、カートをリセットする
+      if params[:checkout_type] == 'register'
+        # 決済が完了したのでカートをリセットする
+        reset_cart
+      end
     rescue Stripe::InvalidRequestError => e
       logger.error(e)
-      # TODO: root_pathに変更する
-      redirect_to cart_path
+      redirect_to root_path
       flash[:alert] = '無効なページです'
     end
   end
 
   private
 
-  def generate_line_items
+  def generate_line_items(product_id_quantity_pair)
     line_items = []
-    cart = current_cart
-    cart.each do |product_id, quantity|
+    product_id_quantity_pair.each do |product_id, quantity|
       product = Product.find_by(id: product_id.to_i)
 
       next unless product
@@ -60,7 +60,7 @@ class Customer::CheckoutsController < ApplicationController
     line_items
   end
 
-  def success_url
+  def generate_success_url(checkout_type)
     if Rails.env.production?
       scheme = 'https'
       host = request.domain
@@ -69,6 +69,25 @@ class Customer::CheckoutsController < ApplicationController
       domain = 'http://localhost:3000'
     end
 
-    "#{domain}/checkout/success?session_id={CHECKOUT_SESSION_ID}"
+    "#{domain}/checkout/success?session_id={CHECKOUT_SESSION_ID}&checkout_type=#{checkout_type}"
+  end
+
+  def create_checkout_session(customer, line_items, success_url)
+    Stripe::Checkout::Session.create({
+                                       client_reference_id: customer.present? ? customer.id : nil,
+                                       customer: customer.present? ? customer&.stripe_customer_id : nil,
+                                       line_items:,
+                                       mode: 'payment',
+                                       #  ユーザーがCheckoutで戻るボタンをクリックするとこのページにリダイレクトされるURL
+                                       cancel_url: cart_url,
+                                       #  決済が完了したときにリダイレクトされるURL
+                                       success_url:,
+                                       #  TODO:デジタル商品のみの場合はshipping_address_collectionを不要なので削除する
+                                       shipping_address_collection: {
+                                         allowed_countries: %w[JP]
+                                       },
+                                       # requiredにしないと、請求先は国だけ選べるという微妙な見た目になってしまう
+                                       billing_address_collection: 'required'
+                                     })
   end
 end
