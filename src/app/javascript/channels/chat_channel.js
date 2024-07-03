903395ff4e36e81e7cc9e77638c62c228d867297
seiichi0404 <yamayamaseisei1834@gmail.com>
2024-07-04 08:51:38 +0900

    チャットリストの追加

--- src/app/javascript/channels/chat_channel.js
+++ src/app/javascript/channels/chat_channel.js
@@ -10,6 +10,41 @@ consumer.subscriptions.create("ChatChannel", {
   },
 
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
+    // ブロードキャストされたデータをキャッチ
+    console.log(data.chat)
+    if (data.action === 'create') {
+      addChat(data.chat, data.customer_account);
+    }
   }
 });
+
+
+function addChat(chat, customerAccount) {
+  const chatList = document.querySelector('tbody');
+  const chatRow = document.createElement('tr');
+  chatRow.className = 'bg-gre border-b bg-white hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:border-gray-700 dark:bg-gray-800';
+  chatRow.id = `chat-${chat.id}`;
+  chatRow.innerHTML = `
+    <td class="px-6 py-4 font-semibold dark:text-white">${customerAccount.email}</td>
+    <td class="px-6 py-4 dark:text-white">${new Date(chat.created_at).toLocaleString()}</td>
+    <td class="px-6 py-4 dark:text-white">
+      <div class="flex items-center">
+        ${chat.status === 'waiting_for_admin' ? `
+          <div class="flex items-center dark:text-white">
+            <div class="me-2 h-2.5 w-2.5 rounded-full bg-yellow-500"></div>
+            未対応
+          </div>
+        ` : `
+          <div class="flex items"center dark:text-white">
+            <div class="me-2 h-2.5 w-2.5 rounded-full bg-green-500"></div>
+            対応中
+          </div>
+        `}
+      </div>
+    </td>
+    <td class="space-x-2 px-2 py-4 md:space-x-10 md:px-6">
+      <a href="/admin/chats/${chat.id}" data-turbo="false" class="font-medium text-sky-600 hover:underline">接続する</a>
+    </td>
+  `;
+  chatList.appendChild(chatRow);
+}
