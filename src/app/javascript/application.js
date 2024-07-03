4b8d20fc9e501e120bf8ebde2dcd5ec82338a7ba
seiichi0404 <yamayamaseisei1834@gmail.com>
2024-07-04 08:16:23 +0900

    読み込みファイルの設定

--- src/app/javascript/application.js
+++ src/app/javascript/application.js
@@ -1,4 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "./controllers"
-import "flowbite/dist/flowbite.turbo.js"import "./channels"
+import "flowbite/dist/flowbite.turbo.js"
+import "./channels"
