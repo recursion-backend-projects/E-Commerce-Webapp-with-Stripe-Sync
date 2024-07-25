4dfa25b50fea812c3256af48c36b905c75da1fb1
seiichi0404 <yamayamaseisei1834@gmail.com>
2024-07-25 21:54:16 +0900

    微調整

--- src/app/controllers/home_controller.rb
+++ src/app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     # アピールしたい商品はここで調整する
+    @customer = true
     @featured_products = Product.where(status: :published).order(updated_at: :desc).limit(3)
     @popular_products = Product.where(status: :published).order('RAND()').limit(3)
   end
