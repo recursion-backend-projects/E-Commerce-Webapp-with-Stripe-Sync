0d3fa2d6831ff699ab27c42519428bf6eb419e5e
seiichi0404 <yamayamaseisei1834@gmail.com>
2024-07-25 09:15:37 +0900

    各セクションを動的に表示

--- src/app/controllers/home_controller.rb
+++ src/app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @customer = true
+    # アピールしたい商品はここで調整する
+    @featured_products = Product.where(status: :published).order(updated_at: :desc).limit(3)
+    @popular_products = Product.where(status: :published).order("RAND()").limit(3)
   end
 end
